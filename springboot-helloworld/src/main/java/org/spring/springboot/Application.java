1aab66b5fee8474e38438ab03e020884137dcdf1
lichaoi <lichaoi@yonyou.com>
2018-05-24 14:41:53 +0800

    logback 日志

--- springboot-helloworld/src/main/java/org/spring/springboot/Application.java
+++ springboot-helloworld/src/main/java/org/spring/springboot/Application.java
@@ -1,10 +1,13 @@
 package org.spring.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Spring Boot 应用启动类
@@ -13,7 +16,11 @@ import org.springframework.context.annotation.Bean;
  */
 // Spring Boot 应用的标识
 @SpringBootApplication
+// 开启定时任务
+@EnableScheduling
 public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         // 程序启动入口
@@ -26,9 +33,11 @@ public class Application {
         return args -> {
             System.out.println("Let's inspect the beans provided by Spring Boot:");
             String[] beanNames = ctx.getBeanDefinitionNames();
+			logger.info("Let's inspect the beans provided by Spring Boot:");
             //Arrays.sort(beanNames);
             for (String beanName : beanNames) {
                 System.out.println(beanName);
+				logger.info(beanName);
             }
         };
     }
---
+++ springboot-helloworld/src/main/java/org/spring/springboot/web/ScheduledTasks.java
@@ -0,0 +1,21 @@
+// package org.spring.springboot.web;
+//
+// import org.springframework.scheduling.annotation.Scheduled;
+//
+// import java.text.SimpleDateFormat;
+// import java.util.Date;
+//
+/// **
+// * @author lichaoi
+// * @date 2018年05月23日 20:50 <br/>
+// * 说明： 定时任务
+// */
+// public class ScheduledTasks {
+//
+// @Scheduled(cron = "/5")
+// public void reportCurrentTime() {
+// SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+// System.out.println(ScheduledTasks.class + " 定时任务" + dateFormat.format(new
+// Date()));
+// }
+// }
