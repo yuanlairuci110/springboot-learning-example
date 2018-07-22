0f4567292e95805666c99699d03cd4e73486c0f0
lichaoi <lichaoi@yonyou.com>
2018-07-22 17:21:35 +0800

    热部署

--- springboot-helloworld/src/main/java/org/spring/springboot/web/HelloWorldController.java
+++ springboot-helloworld/src/main/java/org/spring/springboot/web/HelloWorldController.java
@@ -1,5 +1,6 @@
 package org.spring.springboot.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,15 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String sayHello() {
         return "Hello,World!";
+    }
+
+    @GetMapping("/test1")
+    public String say_helloWorld(){
+        return "hello world";
+    }
+
+    @GetMapping("/test2")
+    public String say_helloWorld2(){
+        return "hello world 222222222";
     }
 }
